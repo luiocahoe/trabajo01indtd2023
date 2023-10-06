fe753457f865122e20c5e1b4db26ef2c77544a82
luiocahoe <luisocanahober@gmail.com>
2023-10-06 16:48:41 +0200

    Creación del documento donde solucionaremos ambos problemas

---
+++ Problema_individual.R
@@ -0,0 +1,8 @@
+###############################################################################
+# PRIMER TRABAJO TEORÍA DE LA DECISIÓN 
+# Luis Carlos Ocaña Hoeber                                           07/10/2023
+###############################################################################
+
+rm(list=ls()) # Elimina todas las variables/objetos almacenados
+source("teoriadecision_funciones_incertidumbre.R") # Lectura de las funciones 
+# creadas por "calote" en GitHub
