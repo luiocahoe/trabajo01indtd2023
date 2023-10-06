077c335ae3362df3daae453e2134e874b452dce2
luiocahoe <luisocanahober@gmail.com>
2023-10-06 16:51:49 +0200

    Generación de datos aleatorios necesarios

--- Problema_individual.R
+++ Problema_individual.R
@@ -5,4 +5,14 @@
 
 rm(list=ls()) # Elimina todas las variables/objetos almacenados
 source("teoriadecision_funciones_incertidumbre.R") # Lectura de las funciones 
-# creadas por "calote" en GitHub
+# creadas por "calote" en GitHub.
+
+#### PRIMER APARTADO. Tabla de decisión con valores inventados y resuelto con
+# cada uno de los métodos o funciones individuales de Incertidumbre por
+# separado (tanto en situación favorable como desfavorable)
+
+set.seed(2601) # Semilla para reproductividad
+datos1=runif(4*7) # Creación de datos aleatorios
+datos1 # Visualización de datos
+tabla1=crea.tablaX(datos1,7,4) # Creación de la tabla
+tabla1 # Visualización de tabla
