2abbe45616c79ba86d1eb9a2074cc0564e60ec41
luiocahoe <luisocanahober@gmail.com>
2023-10-06 16:54:11 +0200

    Solución con criterio de Wald y optimísta

--- Problema_individual.R
+++ Problema_individual.R
@@ -16,3 +16,33 @@ datos1=runif(4*7) # Creación de datos aleatorios
 datos1 # Visualización de datos
 tabla1=crea.tablaX(datos1,7,4) # Creación de la tabla
 tabla1 # Visualización de tabla
+
+### Criterio de Wald o pesimísta
+## Favorable
+WaldF=criterio.Wald(tabla1,T)
+
+cat("La alternativa óptima por el criterio ",WaldF$criterio, " en el caso de ", 
+    ifelse(WaldF$metodo=="favorable","beneficios","costes"),
+    " es la ",names(WaldF$AlternativaOptima),
+    " con un valor óptimo de ", WaldF$ValorOptimo, sep = "" )
+## Desfavorable
+WaldD=criterio.Wald(tabla1,F)
+cat("La alternativa óptima por el criterio ",WaldD$criterio, " en el caso de ",
+    ifelse(WaldD$metodo=="favorable","beneficios","costes"),
+    " es la ",names(WaldD$AlternativaOptima),
+    " con un valor óptimo de ", WaldD$ValorOptimo, sep = "" )
+
+### Criterio optimista
+## Favorable
+OptimistaF=criterio.Optimista(tabla1,T)
+cat("La alternativa óptima por el criterio ",OptimistaF$criterio, " en el caso de ",
+    ifelse(OptimistaF$metodo=="favorable","beneficios","costes"),
+    " es la ",names(OptimistaF$AlternativaOptima),
+    " con un valor óptimo de ", OptimistaF$ValorOptimo, sep = "" )
+## Desfavorable
+OptimistaD=criterio.Optimista(tabla1,F)
+cat("La alternativa óptima por el criterio ",OptimistaD$criterio, " en el caso de ",
+    ifelse(OptimistaD$metodo=="favorable","beneficios","costes"),
+    " es la ",names(OptimistaD$AlternativaOptima),
+    " con un valor óptimo de ", OptimistaD$ValorOptimo, sep = "" )
+
